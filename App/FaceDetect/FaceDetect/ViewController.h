5a0634383cae65f1625504baa3c36b49631c25b3
Ichi Kanaya <kanaya@pineapple.cc>
2018-03-02 12:55:31 +0900

    Added CoreImage Framework. Fixed warning.

--- App/FaceDetect/FaceDetect/ViewController.h
+++ App/FaceDetect/FaceDetect/ViewController.h
@@ -9,7 +9,7 @@
 #import <Cocoa/Cocoa.h>
 #import <AVFoundation/AVFoundation.h>
 
-@interface ViewController: NSViewController
+@interface ViewController: NSViewController <AVCaptureVideoDataOutputSampleBufferDelegate>
 @property (nonatomic, strong) AVCaptureSession *session;
 @property (nonatomic, strong) NSArray *videoDevices;
 @property (nonatomic, strong) IBOutlet NSImageView *imageView;
